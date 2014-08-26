b7b9ec7adc7d846498ecb4bccb2f5cae43b93007
Wil Cooley <wcooley@nakedape.cc>
2014-08-25 22:50:02 -0700

    Correct test for getmaxnodelen
    The previous behavior must have been a bug from a previous release.

--- tests/genders_core.py
+++ tests/genders_core.py
@@ -96,11 +96,8 @@ class TestGendersGetNums(unittest.TestCase):
     def test_getmaxattrs(self):
         self.assertEqual(self.genders.getmaxattrs(), 2)
 
-    # I have been unable to ascertain why or where, but getmaxnodelen seems to
-    # return 13 as a minimum. I experimentally confirmed that the C library
-    # does this itself and it is not some weird misunderstanding with ctypes.
     def test_getmaxnodelen(self):
-        self.assertEqual(self.genders.getmaxnodelen(), 13)
+        self.assertEqual(self.genders.getmaxnodelen(), 5)
 
     def test_getmaxattrlen(self):
         self.assertEqual(self.genders.getmaxattrlen(), 9)
